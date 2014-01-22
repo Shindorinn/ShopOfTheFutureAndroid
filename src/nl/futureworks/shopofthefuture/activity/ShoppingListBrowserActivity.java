d7b605e0c3c53feaa5a8111e43e0f62863b9d9bf
Arz88 <arjan_strijland@hotmail.com>
2014-01-22 16:35:10 +0100

    Mock shopping list browser added

---
+++ src/nl/futureworks/shopofthefuture/activity/ShoppingListBrowserActivity.java
@@ -0,0 +1,89 @@
+package nl.futureworks.shopofthefuture.activity;
+
+import java.util.ArrayList;
+
+import nl.futureworks.shopofthefuture.android.widget.PullToRefreshListView;
+import nl.futureworks.shopofthefuture.android.widget.PullToRefreshListView.OnRefreshListener;
+
+import com.futureworks.shopofthefuture.R;
+
+import android.os.AsyncTask;
+import android.os.Bundle;
+import android.app.Activity;
+import android.view.Menu;
+import android.widget.ArrayAdapter;
+
+public class ShoppingListBrowserActivity extends Activity {
+	
+	//Custom pull refresh ListView
+	private PullToRefreshListView browserListView;
+	
+	//Mocks shopping list names
+	private ArrayList<String> mockNameArray;
+	private ArrayAdapter<String> adapter;
+	
+	//Dummycounter
+	private int mockCounter = 0;
+	
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_shopping_list_browser);
+		
+		initializeListView();
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.shopping_list_browser, menu);
+		return true;
+	}
+	
+	/**
+	 * Initializes the shopping list browser and sets the adapter
+	 */
+	private void initializeListView(){
+		browserListView = (PullToRefreshListView) findViewById(R.id.shoppinglistbrowser_listview);
+		
+		//Initialize itemArray and Adapter
+				mockNameArray = new ArrayList<String>();
+				adapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, mockNameArray);
+				browserListView.setAdapter(adapter);
+				
+		//Set OnRefreshListener
+		browserListView.setOnRefreshListener(new OnRefreshListener() {
+			@Override
+			public void onRefresh() {
+				new GetShoppingListsTask().execute();
+			}
+		});
+	}
+	
+	private class GetShoppingListsTask extends AsyncTask<Void, Void, ArrayList<String>> {
+		
+		@Override
+		protected ArrayList<String> doInBackground(Void... params) {
+			//Simulate load of data
+			try{
+				Thread.sleep(2000);
+			} catch (InterruptedException e) {
+				e.printStackTrace();
+			}
+			mockNameArray.add("List " + mockCounter);
+			mockCounter++;
+			return mockNameArray;
+		}		
+		
+		@Override
+		protected void onPostExecute(ArrayList<String> result) {
+			browserListView.onRefreshComplete();
+			
+			super.onPostExecute(result);
+			
+		}
+	}
+
+}
+
