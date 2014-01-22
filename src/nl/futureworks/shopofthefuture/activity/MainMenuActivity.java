70d7d1be4a48080c229bc9092e25e346df4ad57a
啊Denko啊 <juss4dabanta@gmail.com>
2014-01-22 13:32:17 +0100

    Fixed manifest and added barcode scanner support

--- src/nl/futureworks/shopofthefuture/activity/MainMenuActivity.java
+++ src/nl/futureworks/shopofthefuture/activity/MainMenuActivity.java
@@ -1,23 +1,37 @@
 package nl.futureworks.shopofthefuture.activity;
 
-import com.futureworks.shopofthefuture.R;
-
-import android.os.Bundle;
 import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import com.futureworks.shopofthefuture.R;
+import com.google.zxing.integration.android.IntentIntegrator;
+import com.google.zxing.integration.android.IntentResult;
 
 public class MainMenuActivity extends Activity {
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
 		this.initializeActivity();
 	}
-
-	
-	
-	
 	
 	private void initializeActivity(){
 		this.setContentView(R.layout.activity_main_menu);
 	}
+
+    public void scanBarcode(View v)
+    {
+        IntentIntegrator integrator = new IntentIntegrator(this);
+        integrator.initiateScan();
+    }
+
+    public void onActivityResult(int requestCode, int resultCode, Intent intent) {
+        IntentResult scanResult = IntentIntegrator.parseActivityResult(requestCode, resultCode, intent);
+        if (scanResult != null) {
+            Log.d("DEBUG", scanResult.getContents());
+        }
+    }
 
 }
